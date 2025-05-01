2bf77d71d63ce475d0177ec7d0526c2fa106313f
Kevin Marchais <kevinmarchais@gmail.com>
2025-05-01 11:55:29 +0200

    refactor: remove unused geojson processing functions in WorldCountries component

--- src/components/WorldCountries/WorldCountries.vue
+++ src/components/WorldCountries/WorldCountries.vue
@@ -5,7 +5,6 @@
     geojson-url="https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson"
     geojson-name-property="name"
     :map-options="mapOptions"
-    :process-geojson-data-fn="processWorldData"
     :total-rounds-override="54"
   />
 </template>
@@ -13,8 +12,6 @@
 <script setup lang="ts">
 import MapGame from '../MapGame.vue';
 import L from 'leaflet';
-import type { FeatureCollection, Geometry } from 'geojson';
-import type { GeoJSONProperties } from '../../utils/geojsonUtils';
 
 const mapOptions = {
   initialCenter: [20, 0] as L.LatLngExpression,
@@ -24,12 +21,4 @@ const mapOptions = {
   worldCopyJump: true,
   maxBoundsViscosity: 1.0
 };
-
-const processWorldData = (data: FeatureCollection<Geometry, GeoJSONProperties>) => {
-  return createWorldWrapping(data);
-};
-
-function createWorldWrapping(featureCollection: FeatureCollection<Geometry, GeoJSONProperties>) {
-  return featureCollection;
-}
 </script>
